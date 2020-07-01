3e76bec415971da645ee33715bf30bac5105a6d2
Raghu Chandan <33779280+Raghu-Chandan@users.noreply.github.com>
2020-07-01 10:19:54 +0530

    Adding a new API method for the backend joke retreival

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger.backend;
 
+import com.example.javajokes.JokeSmith;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -23,6 +24,16 @@ public class MyEndpoint {
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
         response.setData("Hi, " + name);
+
+        return response;
+    }
+
+    @ApiMethod(name = "getJokeFromCloud")
+    public MyBean getJokeFromCloud() {
+        MyBean response = new MyBean();
+        JokeSmith jokeSmith = new JokeSmith();
+        String joke = jokeSmith.getJoke();
+        response.setData(joke);
 
         return response;
     }
